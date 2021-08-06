dc0a1891eb59f27abbf2f083a44f8b791c2cf3cc
Blayne Chard <blayne@chard.com>
2021-08-06 18:08:43 +1200

    feat: record issue comments

--- src/hook.pr.comment.ts
+++ src/hook.pr.comment.ts
@@ -35,38 +35,3 @@ export const PullRequestCommentAction: HookAction<PullRequestReviewCommentEvent>
     };
   },
 };
-
-// export const IssueComment: HookAction<IssueComment> = {
-//   name: 'pull_request_review_comment',
-//   is(type: string, e: WebHookEnterpriseEvent): e is PullRequestReviewCommentEvent {
-//     return type === 'pull_request_review_comment';
-//   },
-
-//   process(hook: PullRequestReviewCommentEvent): HookIndex | null {
-//     const orgId = hook.organization?.login ?? 'unknown';
-
-//     return {
-//       index: indexName(orgId, 'issue', hook.comment.created_at),
-//       body: {
-//         '@timestamp': hook.comment.created_at,
-//         '@type': 'pull_request_review_comment',
-//         ...hook,
-//         comment: {
-//           ...hook.comment,
-//           user: hook.comment.user.login,
-//           _links: undefined,
-//         },
-//         pull_request: {
-//           url: hook.pull_request.html_url,
-//           number: hook.pull_request.number,
-//           user: hook.pull_request.user.login,
-//         },
-//         repository: hook.repository.full_name,
-//         sender: hook.sender.login,
-//         organization: orgId,
-//         enterprise: undefined,
-//         computed: {},
-//       },
-//     };
-//   },
-// };
