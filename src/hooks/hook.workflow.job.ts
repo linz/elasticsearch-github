3b8e0fc4b0781032c93024688b4ca4f890044701
Blayne Chard <blayne@chard.com>
2021-08-09 17:03:18 +1200

    fix: correct event name for workflows

--- src/hooks/hook.workflow.job.ts
+++ src/hooks/hook.workflow.job.ts
@@ -2,13 +2,14 @@ import { WorkflowRunEvent } from '@octokit/webhooks-types';
 import { HookAction, HookIndex, WebhookEnterprise, WebHookEnterpriseEvent } from '../hook';
 
 export const WorkflowJobAction: HookAction<WorkflowRunEvent> = {
-  name: 'workflow_run',
+  name: 'workflow_job',
   is(type: string, e: WebHookEnterpriseEvent): e is WorkflowRunEvent {
-    return type === 'workflow_run';
+    return type === 'workflow_job';
   },
 
   process(hook: WorkflowRunEvent & WebhookEnterprise): HookIndex | null {
     const updatedAt = hook.workflow_run.updated_at ?? new Date().toISOString();
+    if (hook.action !== 'completed') return null;
     return { prefix: 'workflow', timestamp: updatedAt, hook };
   },
 };
